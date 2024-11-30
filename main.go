cb42ada2f98dd13e794f62a6462894a5de4f7eca
kaium123 <mohammadkaiom79@gmail.com>
2024-11-30 11:49:34 +0600

    add queue

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+// Custom deque implementation
+type Deque struct {
+	data [][]int
+}
 
-func main() {
-	fmt.Println("hieiufyewiu")
+func (d *Deque) PushBack(val []int) {
+	d.data = append(d.data, val)
+}
+
+func (d *Deque) PushFront(val []int) {
+	d.data = append([][]int{val}, d.data...)
+}
+
+func (d *Deque) PopFront() []int {
+	if len(d.data) == 0 {
+		return nil
+	}
+	val := d.data[0]
+	d.data = d.data[1:]
+	return val
+}
+
+func (d *Deque) Len() int {
+	return len(d.data)
 }
