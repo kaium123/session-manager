c0edf9b4c0d9b8f0152db2d508a8d5904bc4edd4
kaium123 <mohammadkaiom79@gmail.com>
2024-11-30 11:48:36 +0600

    first commit

---
+++ server.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/hi" {
+		fmt.Fprintf(w, "hello")
+	} else {
+		http.NotFound(w, r)
+	}
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	fmt.Println("Server is listening on port 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+}
